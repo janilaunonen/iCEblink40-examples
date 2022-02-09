d736494ed84af944f9df5ca6ae83806f7ab73afc
janilaunonen <janilaunonen@nowhere>
2022-02-09 20:49:28 +0200

    Fix divider module
    Fixed divider module so that it passes through every #COUNT
    input_clk pulse.
    
    Added a guard to check that #COUNT is sensible value.
    
    Added few test cases to divider_tb.sv

--- common-modules/divider.sv
+++ common-modules/divider.sv
@@ -1,22 +1,21 @@
+module divider
+#(parameter COUNT = 3300000)
+ (input logic clk, input_clk, output logic output_pulse);
 
+// check that the parameter is sensible i.e. > 1
+initial if (COUNT < 2)
+	$fatal(1, "Divider given nonsensical parameter value (<2)");
 
-module divider #(parameter COUNT = 3300000)
-(input logic clk, input logic input_clk, output logic output_pulse);
-logic [$clog2(COUNT):0] counter = 0;
+logic [$clog2(COUNT - 1):0] counter = 0;
 logic output_clk = 0;
-initial output_clk = 0;
 
-always_ff @(posedge clk)
-begin
-	if (input_clk == 1'b1)
-	begin
-		if (counter != COUNT)
-		begin
+always_ff @(posedge clk) begin
+	if (input_clk == 1'b1) begin
+		if (counter != (COUNT - 1)) begin
 			counter++;
 			output_clk <= 1'b0;
 		end
-		else
-		begin
+		else begin
 			counter <= 0;
 			output_clk <= 1'b1;
 		end
--- common-modules/divider_tb.sv
+++ common-modules/divider_tb.sv
@@ -1,14 +1,22 @@
-module divider_tb();
-logic clk, output_pulse;
+module divider_tb;
+logic clk, input_clk, output_pulse0, output_pulse1, output_pulse2, output_pulse3, output_pulse4;
 
-divider #(.COUNT(2)) mut (.clk(clk), .input_clk(clk), .output_pulse(output_pulse));
+//divider #(.COUNT(0)) mut_zero (.clk(clk), .input_clk(clk), .output_pulse(output_pulse0));
+//divider #(.COUNT(1)) mut_half (.clk(clk), .input_clk(clk), .output_pulse(output_pulse1));
+divider #(.COUNT(2)) mut_third (.clk(clk), .input_clk(clk), .output_pulse(output_pulse2));
+divider #(.COUNT(3)) mut_fourth (.clk(clk), .input_clk(clk), .output_pulse(output_pulse3));
+divider #(.COUNT(4)) mut_fifth (.clk(clk), .input_clk(clk), .output_pulse(output_pulse4));
 
 // oscillate clock every 5 simulation units
-always #5 clk <= !clk;
+always #5 clk <= ~clk;
+
+// oscillate input_clk every 10 simulation units
+always #10 input_clk <= ~input_clk;
 
 initial #0 begin
 	clk = 0;
-#60     $finish;
+	input_clk = 0;
+#100     $finish;
 end
 
 initial begin
